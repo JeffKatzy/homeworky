79b730b2bcc54e735e9015c147e248f4d183778c
Jeffrey Katz <JeffreyEricKatz@gmail.com>
2013-02-18 17:30:53 -0500

    changing sms create method

--- app/controllers/sms_controller.rb
+++ app/controllers/sms_controller.rb
@@ -5,7 +5,8 @@ class SmsController < ApplicationController
   def create
     @sms = Sms.create(incoming_number: params['From'].to_s,
       content_received: params['Body'].downcase, call_sid: params['CallSid'].to_s)
-    @user = @sms.find_user_from_number
-    @sms.respond @user
+    @sms.save
+    # @user = @sms.find_user_from_number
+    # @sms.respond @user
   end
 end
